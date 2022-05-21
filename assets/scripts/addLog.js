c7eb6daed28cb0b4f23b9603fa74ae6e7cfd9e32
Marius Žalkauskas <ma.zalkauskas@gmail.com>
2022-05-22 01:11:12 +0300

    Add log func added

---
+++ assets/scripts/addLog.js
@@ -0,0 +1,40 @@
+const baseURL = "http://localhost:8080";
+const addLogForm = document.forms.addLog;
+const token = localStorage.getItem("token");
+const id = localStorage.getItem("pet_id");
+
+const displayMsg = (message) => {
+	const msgContainer = document.querySelector(".msgContainer");
+	msgContainer.innerHTML = message;
+	msgContainer.classList.add("msgShow");
+};
+
+const addLog = async (userData) => {
+	const res = await fetch(`${baseURL}/v1/logs/`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify(userData),
+	});
+	const data = await res.json();
+
+	if (data.err) {
+		return displayMsg(data.err);
+	}
+
+	location.replace("logs.html");
+};
+
+addLogForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const pet_id = Number(id);
+	const visit_type = event.target.elements.visit_type.value;
+	const description = event.target.elements.description.value;
+
+	console.log({ pet_id, visit_type, description });
+
+	addLog({ pet_id, visit_type, description });
+});
